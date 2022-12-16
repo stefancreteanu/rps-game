da4b72adb7f34078f4b237cb4fd91adf38e196ad
stefancreteanu <65374421+stefancreteanu@users.noreply.github.com>
2022-12-17 01:06:11 +0200

    done

--- index.js
+++ index.js
@@ -18,9 +18,11 @@ rock.onclick = () => {
     } else if (playerWeapon == 'rock' && weapons[computerWeapon] == 'paper') {
         document.getElementById('message').innerHTML = `Computer has chosen ${weapons[computerWeapon]}, computer wins the match`;
         document.getElementById('computerScore').innerHTML = computerScore += 1;
+        localStorage.setItem('computerScore', computerScore);
     } else if (playerWeapon == 'rock' && weapons[computerWeapon] == 'scissors') {
         document.getElementById('message').innerHTML = `Computer has chosen ${weapons[computerWeapon]}, player wins the match`;
         document.getElementById('playerScore').innerHTML = playerScore += 1;
+        localStorage.setItem('playerScore', playerScore);
     }
     console.log(playerWeapon);
     console.log(weapons[computerWeapon]);
@@ -34,9 +36,11 @@ paper.onclick = () => {
     } else if (playerWeapon == 'paper' && weapons[computerWeapon] == 'scissors') {
         document.getElementById('message').innerHTML = `Computer has chosen ${weapons[computerWeapon]}, computer wins the match`;
         document.getElementById('computerScore').innerHTML = computerScore += 1;
+        localStorage.setItem('computerScore', computerScore);
     } else if (playerWeapon == 'paper' && weapons[computerWeapon] == 'rock') {
         document.getElementById('message').innerHTML = `Computer has chosen ${weapons[computerWeapon]}, player wins the match`;
         document.getElementById('playerScore').innerHTML = playerScore += 1;
+        localStorage.setItem('playerScore', playerScore);
     }
     console.log(playerWeapon);
     console.log(weapons[computerWeapon]);
@@ -50,15 +54,29 @@ scissors.onclick = () => {
     } else if (playerWeapon == 'scissors' && weapons[computerWeapon] == 'rock') {
         document.getElementById('message').innerHTML = `Computer has chosen ${weapons[computerWeapon]}, computer wins the match`;
         document.getElementById('computerScore').innerHTML = computerScore += 1;
+        localStorage.setItem('computerScore', computerScore);
     } else if (playerWeapon == 'scissors' && weapons[computerWeapon] == 'paper') {
         document.getElementById('message').innerHTML = `Computer has chosen ${weapons[computerWeapon]}, player wins the match`;
         document.getElementById('playerScore').innerHTML = playerScore += 1;
+        localStorage.setItem('playerScore', playerScore);
     }
     console.log(playerWeapon);
     console.log(weapons[computerWeapon]);
 }
 
+window.onload = () => {
+    document.getElementById('playerScore').innerHTML = localStorage.getItem('playerScore');
+    document.getElementById('computerScore').innerHTML = localStorage.getItem('computerScore');
+    playerScore = parseInt(localStorage.getItem('playerScore'));
+    computerScore = parseInt(localStorage.getItem('computerScore'));
+}
+
 const resetScore = () => {
-    document.getElementById('computerScore').innerHTML = 0;
-    document.getElementById('playerScore').innerHTML = 0;
+    playerScore = 0;
+    computerScore = 0;
+    localStorage.setItem('playerScore', 0);
+    localStorage.setItem('computerScore', 0);
+    document.getElementById('computerScore').innerHTML = localStorage.getItem('playerScore');
+    document.getElementById('playerScore').innerHTML = localStorage.getItem('computerScore');
+    document.getElementById('message').innerHTML = '';
 }
