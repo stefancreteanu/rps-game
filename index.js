94f47d052676af27d1f17eddd82ce308f1e124f4
stefancreteanu <65374421+stefancreteanu@users.noreply.github.com>
2022-12-20 01:24:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ let playerWeapon = '';
 let playerScore = 0;
 let computerScore = 0;
 
+console.log(playerScore, computerScore);
+
 const computerChoice = () => {
     computerWeapon = Math.floor(Math.random() * weapons.length);
 }
@@ -20,12 +22,12 @@ rock.onclick = () => {
     if(playerWeapon == weapons[computerWeapon]) {
         document.getElementById('message').innerHTML = 'Tie!';
     } else if (weapons[computerWeapon] == 'paper') {
-        document.getElementById('message').innerHTML = `Computer has chosen ${weapons[computerWeapon]}, computer wins the match!`;
         document.getElementById('computerScore').innerHTML = computerScore += 1;
+        document.getElementById('message').innerHTML = `Computer has chosen ${weapons[computerWeapon]}, computer wins the match!`;
         localStorage.setItem('computerScore', computerScore);
     } else if (weapons[computerWeapon] == 'scissors') {
-        document.getElementById('message').innerHTML = `Computer has chosen ${weapons[computerWeapon]}, player wins the match!`;
         document.getElementById('playerScore').innerHTML = playerScore += 1;
+        document.getElementById('message').innerHTML = `Computer has chosen ${weapons[computerWeapon]}, player wins the match!`;
         localStorage.setItem('playerScore', playerScore);
     }
 }
@@ -63,6 +65,10 @@ scissors.onclick = () => {
 }
 
 window.onload = () => {
+    if(localStorage.getItem('playerScore') == null && localStorage.getItem('computerScore') == null) {
+        localStorage.setItem('playerScore', 0);
+        localStorage.setItem('computerScore', 0);
+    }
     document.getElementById('playerScore').innerHTML = localStorage.getItem('playerScore');
     document.getElementById('computerScore').innerHTML = localStorage.getItem('computerScore');
     playerScore = parseInt(localStorage.getItem('playerScore'));
